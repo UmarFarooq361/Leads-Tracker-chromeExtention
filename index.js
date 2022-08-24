f1ff3d5a85fbc66923b9c21f4fdb9f2a220097e4
umar farooq <umar@gmail.com>
2022-08-24 23:49:42 +0500

    first version complete

---
+++ index.js
@@ -0,0 +1,48 @@
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+const saveBtn=document.getElementById("save-btn")
+const tabBtn=document.getElementById("tab-btn")
+const deleteBtn=document.getElementById("dlt-btn")
+const ulEl=document.getElementById("ul-el")
+const leadFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+
+if(leadFromLocalStorage){
+    myLeads=leadFromLocalStorage
+    rander(myLeads)
+}
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true} ,function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        rander(myLeads)
+    })
+})
+
+function rander(leads)
+{ 
+    let listItems = ""
+    for(let i=0; i<leads.length;i++){
+        listItems += `
+                <li>
+                    <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                    </a>
+                </li>
+                `
+    }
+    ulEl.innerHTML=listItems
+}
+
+deleteBtn.addEventListener("click",function(){
+    localStorage.clear()
+    myLeads=[]
+    rander(myLeads)
+})
+
+saveBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value)
+    inputEl.value=" "
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    rander(myLeads) 
+})
